a024e257a73cb83ee182ed28ffe65a3e02d9c309
Chirantha Chandima <chiranthachandima@Chiranthas-MacBook-Pro.local>
2024-02-19 20:48:01 +0530

    first commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,88 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import ParkIcon from '@mui/icons-material/Park';
+import SpaIcon from '@mui/icons-material/Spa';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import { Link } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
+import { useContext } from "react";
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+
+const Sidebar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">CropDoc Admin</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <Link to="/" style={{ textDecoration: "none" }}>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          </Link>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <SpaIcon className="icon" />
+              <span>Disease</span>
+            </li>
+          </Link>
+          <li>
+            <ParkIcon className="icon" />
+            <span>Treatments</span>
+          </li>
+          <li>
+            <InsertDriveFileIcon className="icon" />
+            <span>User Records</span>
+          </li>
+          <li>
+            <PeopleAltIcon className="icon" />
+            <span>Professionals</span>
+          </li>
+          <li>
+            <ReviewsIcon className="icon" />
+            <span>Feedbacks</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "LIGHT" })}
+        ></div>
+        <div
+          className="colorOption"
+          onClick={() => dispatch({ type: "DARK" })}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/formSource.js
@@ -0,0 +1,55 @@
+export const userInputs = [
+    {
+      id: "dname",
+      label: "Disease Name",
+      type: "text",
+      placeholder: "",
+    },
+    {
+      id: "dnumber",
+      label: "Disease Number",
+      type: "number",
+      placeholder: "",
+    },
+    {
+      id: "dcase",
+      label: "Disease Cause",
+      type: "test",
+      placeholder: "",
+    },
+    
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Apple Macbook Pro",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "Computers",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
+  
